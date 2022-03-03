621672ea0374ca2e0fcc3b0fc31a56562a94ea36
How Yu Chern <yhow0004@student.monash.edu>
2022-03-03 17:48:51 +0800

    This is my first code in java

---
+++ Bootcamps/Bootcamp01/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.name = "abc";
+        person.age = 25;
+        person.color = "white";
+
+        person.displayInformation();
+
+    }
+}
---
+++ Bootcamps/Bootcamp01/src/Person.java
@@ -0,0 +1,11 @@
+public class Person{
+
+    String name;
+    int age;
+    String color;
+
+    public void displayInformation(){
+        System.out.println("Person has the name = " + name + ", age is = " + age + ", ant the color is = " + color + ".");
+    }
+
+}
