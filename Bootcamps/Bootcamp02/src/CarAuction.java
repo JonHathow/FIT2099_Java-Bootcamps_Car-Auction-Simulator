037f1ea7e0e298eca211bdac2e44a5f2d1b81e5f
How Yu Chern <yhow0004@student.monash.edu>
2022-03-10 16:57:34 +0800

    Week 2 Bootcamp - Version 1 Complete Author: How Yu Chern (31189733) Date: 10/3/2022

---
+++ Bootcamps/Bootcamp02/src/CarAuction.java
@@ -0,0 +1,34 @@
+public class CarAuction{
+    //Attributes - Car Array of max size 3.
+    private Car [] carArray = new Car[3];
+
+    //Methods
+    //Create Cars method - inserts 3 new cars into carArray.
+    public void createCars() {
+        Car car1 = new Car();
+        car1.createCar("BMW", "X7", 2017);
+        carArray[0] = car1;
+
+        Car car2 = new Car();
+        car2.createCar("Audi", "TT", 2014);
+        carArray[1] = car2;
+
+        Car car3 = new Car();
+        car3.createCar("Chevrolet", "Corvette", 2020);
+        carArray[2] = car3;
+    }
+
+    //Display Car method - display cars in carArray;
+    public void displayCar(){
+        for (int i = 0; i < carArray.length; i++){
+            System.out.print("Car (" + (i + 1) + ") ");
+            carArray[i].getCarDescription();
+        }
+
+    }
+
+    public void printStatus(){
+        System.out.println("Welcome to FIT2099 Car Auction Systems");
+        System.out.println("Thank you for visiting FIT2099 Car Auction System");
+    }
+}
---
+++ Bootcamps/Bootcamp02/src/CarAuctionDriver.java
@@ -0,0 +1,8 @@
+public class CarAuctionDriver{
+    public static void main(String[] args) {
+        CarAuction carAuction1 = new CarAuction();
+        carAuction1.printStatus();
+        carAuction1.createCars();
+        carAuction1.displayCar();
+    }
+}
